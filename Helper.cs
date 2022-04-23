e1054e7b8c6655081a3334c88b0da876b19840c8
David England <david.england.de@googlemail.com>
2022-04-23 13:00:49 +0100

    Implemented namespace

--- Helper.cs
+++ Helper.cs
@@ -11,35 +11,38 @@ using System.Xml.Linq;
 using System.ComponentModel;
 using System.Collections.Generic;
 
-static class Helper
+namespace WaveFunctionCollapse
 {
-    public static int Random(this double[] weights, double r)
+    static class Helper
     {
-        double sum = 0;
-        for (int i = 0; i < weights.Length; i++) sum += weights[i];
-        double threshold = r * sum;
-
-        double partialSum = 0;
-        for (int i = 0; i < weights.Length; i++)
+        public static int Random(this double[] weights, double r)
         {
-            partialSum += weights[i];
-            if (partialSum >= threshold) return i;
+            double sum = 0;
+            for (int i = 0; i < weights.Length; i++) sum += weights[i];
+            double threshold = r * sum;
+
+            double partialSum = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                partialSum += weights[i];
+                if (partialSum >= threshold) return i;
+            }
+            return -1;
         }
-        return -1;
-    }
 
-    public static long ToPower(this int a, int n)
-    {
-        long product = 1;
-        for (int i = 0; i < n; i++) product *= a;
-        return product;
-    }
+        public static long ToPower(this int a, int n)
+        {
+            long product = 1;
+            for (int i = 0; i < n; i++) product *= a;
+            return product;
+        }
 
-    public static T Get<T>(this XElement xelem, string attribute, T defaultT = default)
-    {
-        XAttribute a = xelem.Attribute(attribute);
-        return a == null ? defaultT : (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromInvariantString(a.Value);
-    }
+        public static T Get<T>(this XElement xelem, string attribute, T defaultT = default)
+        {
+            XAttribute a = xelem.Attribute(attribute);
+            return a == null ? defaultT : (T)TypeDescriptor.GetConverter(typeof(T)).ConvertFromInvariantString(a.Value);
+        }
 
-    public static IEnumerable<XElement> Elements(this XElement xelement, params string[] names) => xelement.Elements().Where(e => names.Any(n => n == e.Name));
+        public static IEnumerable<XElement> Elements(this XElement xelement, params string[] names) => xelement.Elements().Where(e => names.Any(n => n == e.Name));
+    }
 }
